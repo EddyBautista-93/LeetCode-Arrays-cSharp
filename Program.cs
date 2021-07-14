912d3a321a0be10c16dd4300520fbb2e26a0c76f
EddyBautista <bautista.j.eddy@gmail.com>
2021-07-14 05:59:48 -0500

    needs work - not returning double 0 after first instance

--- Program.cs
+++ Program.cs
@@ -4,41 +4,46 @@ namespace LeetCode_Arrays_cSharp
 {
     internal class Program
     {
-        //  Given an integer array nums sorted in non-decreasing
-        //  order, return an array of the squares of each number 
-        //  sorted in non-decreasing order.
+        // Duplicate Zeros
 
-        // Example:
+        // Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
 
-        // Input: nums = [-4,-1,0,3,10]
-        // Output: [0,1,9,16,100]
-        // Explanation: After squaring, the array becomes [16,1,0,9,100].
-        // After sorting, it becomes [0,1,9,16,100].
+        // Note that elements beyond the length of the original array are not written.
+
+        // Do the above modifications to the input array in place, do not return anything from your function.
+
+        // Example 1:
+
+        // Input: [
+        // Output: null
+        // Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
         // Example 2:
 
-        // Input: nums = [-7,-3,2,3,11]
-        // Output: [4,9,9,49,121]
+        // Input: [1,2,3]
+        // Output: null
+        // Explanation: After calling your function, the input array is modified to: [1,2,3]
 
-        public static int[] SortedSquares(int[] nums)
+        public static void DuplicateZeros(int[] arr)
         {
-            int n = nums.Length;
-
-            // First convert each array
-            // elements into its square
-            for (int i = 0; i < n; i++)
-                nums[i] = nums[i] * nums[i];
-
-            // Sort an array using
-            // "inbuild sort function"
-            // in Arrays class.
-            Array.Sort(nums);
-            return nums;
+            int len = arr.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if(arr[i] == 0)
+                {
+                    for (int j = len - 1; j >= i + 1; j--)
+                    {
+                        arr[j] = arr[j] - 1;
+                    }
+                    i = i + 1;
+                }
+            }
         }
+
 
         private static void Main(string[] args)
         {
-            int[] testArray = { -4, -1, 0, 3, 10 };
-            SortedSquares(testArray);
+            int[] testArray = { 1, 0, 2, 3, 0, 4, 5, 0};
+            DuplicateZeros(testArray);
         }
     }
 }
